6f879ceafe100af2350bfcf1a0d6b4b6ec5c06d1
OxBryte <silascyrax@gmail.com>
2025-07-12 01:33:47 +0100

    feat: update 2025-07-12 00:33:47

--- src/components/features/Note.jsx
+++ src/components/features/Note.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 
 export default function Note() {
   return (
-    <div>Note</div>
-  )
+      <div className="w-full min-h-[60dvh] rounded-t-3xl py-20 flex items-center justify-center bg-gradient-to-t from-gray-200 to-transparent">
+          
+    </div>
+  );
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Hero from "../components/features/Hero";
+import Note from "../components/features/Note";
 
 export default function Home() {
   return (
     <div>
       <Hero />
+      <Note />
     </div>
   );
 }
