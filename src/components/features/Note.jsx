c40e27fbd0f71c4a00a1f72ba70a0bfbd88b59fc
OxBryte <silascyrax@gmail.com>
2025-07-12 01:39:47 +0100

    feat: update 2025-07-12 00:39:47

--- src/components/features/Note.jsx
+++ src/components/features/Note.jsx
@@ -30,8 +30,8 @@ export default function Note() {
 
   return (
     <div className="w-full min-h-[60dvh] rounded-t-3xl py-20 flex items-center justify-center bg-gradient-to-t from-gray-200 to-transparent">
+      {/* <p className="font-semibold text-2xl">Submit your Idea to the public</p> */}
       <div className="w-full max-w-2xl mx-4">
-      <p className="font-semibold text-2xl">Submit your Idea to the public</p>
         <form onSubmit={handleSubmit} className="space-y-6">
           {/* Note Input Field */}
           <div>
