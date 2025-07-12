1422a5ce196d0a64ee75b33d67bbcceef353b014
OxBryte <silascyrax@gmail.com>
2025-07-12 01:27:47 +0100

    feat: update 2025-07-12 00:27:47

--- src/components/features/Hero.jsx
+++ src/components/features/Hero.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Hero() {
   return (
-    <div className="w-full min-h-[60dvh] py-20 flex items-center justify-center">
+    <div className="w-full min-h-[60dvh] rounded-t-3xl py-20 flex items-center justify-center bg-gradient-to-b from-indigo-50 to-transparent">
       <div className="flex flex-col items-center gap-2">
         <h1 className="text-[48px] font-semibold font-bricolage">
           Make your ideas Onchain!
