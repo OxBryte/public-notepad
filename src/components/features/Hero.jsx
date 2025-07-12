9269218095898477f4e4ddea0cf16d3b486987fb
OxBryte <silascyrax@gmail.com>
2025-07-12 01:21:47 +0100

    feat: update 2025-07-12 00:21:47

--- src/components/features/Hero.jsx
+++ src/components/features/Hero.jsx
@@ -1,11 +1,13 @@
-import React from 'react'
+import React from "react";
 
 export default function Hero() {
   return (
-      <div className='w-full min-h-[60dvh] py-20 flex items-center justify-center'>
-          <div className="flex flex-col items-center gap-3">
-              <h1 className="text-[48px] font-semibold"></h1>
-          </div>
+    <div className="w-full min-h-[60dvh] py-20 flex items-center justify-center">
+      <div className="flex flex-col items-center gap-3">
+        <h1 className="text-[48px] font-semibold font-bricolage">
+          Make your ideas Onchain!
+        </h1>
+      </div>
     </div>
-  )
+  );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+// Supports weights 100-900
+import "@fontsource-variable/inter";
+// Supports weights 200-800
+import '@fontsource-variable/bricolage-grotesque';
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
