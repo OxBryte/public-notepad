4ab20c87f81c88a5756a938ad93040566da38a0b
OxBryte <silascyrax@gmail.com>
2025-07-12 01:18:47 +0100

    feat: update 2025-07-12 00:18:47

--- src/components/features/Hero.jsx
+++ src/components/features/Hero.jsx
@@ -2,6 +2,10 @@ import React from 'react'
 
 export default function Hero() {
   return (
-    <div>Hero</div>
+      <div className='w-full min-h-[60dvh] py-20 flex items-center justify-center'>
+          <div className="flex flex-col items-center gap-3">
+              <h1 className="text-[48px] font-semibold"></h1>
+          </div>
+    </div>
   )
 }
